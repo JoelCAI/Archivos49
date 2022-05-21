597d99386f98a5c64d5de631ac1bdba6e9ff710f
JoelCAI <joelargentina12345@gmail.com>
2022-05-21 15:44:40 -0300

    Agregar archivos de proyecto.

---
+++ Archivos49/Sistema.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos49
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            /* Creo variable de tipo Archivo */
+            TextWriter archivo;
+
+            /* La inicializo y la instancio. Digo que se va a llamar en la pc un archivo con este nombre "HolaMundo.txt" */
+            archivo = new StreamWriter("HolaMundo.txt");
+
+            string mensaje = Validador.ValidarStringNoVacio("Ingrese el texto que será guardado");
+
+            archivo.WriteLine(mensaje);
+
+            archivo.Close();
+
+            Console.WriteLine("Se guardado el archivo " + "*" + mensaje + "*");
+            
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ Archivos49/Validador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos49
+{
+    public class Validador
+    {
+        public static string ValidarStringNoVacio(string mensaje)
+        {
+
+            string opcion;
+            bool valido = false;
+            string mensajeValidador = "\n Puede ser combinación de minúsculas, MAYÚSCULAS y caracteres";
+            string mensajeError = "\n Por favor ingrese un valor no vacio para que pueda continuar. ";
+
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                Console.WriteLine(mensajeValidador);
+
+                opcion = Console.ReadLine();
+
+                if (opcion == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n");
+                    Console.WriteLine(mensajeError);
+                }
+                else
+                {
+                    valido = true;
+                }
+
+            } while (!valido);
+
+            return opcion;
+        }
+    }
+}
